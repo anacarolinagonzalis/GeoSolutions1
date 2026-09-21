5e16d5e3981c95f75938186ed491a296084fa244
Ana Carolina <ana.gonzalis@aluno.cps.sp.gov.br>
2026-09-20 21:48:20 -0300

    Atualização automática do projeto GeoSolutions

--- core/apps.py
+++ core/apps.py
@@ -1,7 +1,19 @@
 
 from django.apps import AppConfig
+from django.contrib.auth.apps import AuthConfig
+
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
     verbose_name = 'Gestão Ambiental'  # Nome que aparecerá no menu do Admin
+
+
+
+class CoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'core'
+    verbose_name = 'Gestão Ambiental'
+
+# Traduz o nome da secção de Autenticação no Admin
+AuthConfig.verbose_name = "Autenticação e Autorização"
