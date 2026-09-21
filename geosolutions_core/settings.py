1139ed22a41b8f7cfd07b86c5fef3cf977a3d219
Ana Carolina <ana.gonzalis@aluno.cps.sp.gov.br>
2026-09-20 21:46:10 -0300

    Atualização automática do projeto GeoSolutions

--- geosolutions_core/settings.py
+++ geosolutions_core/settings.py
@@ -104,13 +104,16 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# Idioma do painel e mensagens do sistema para Português do Brasil
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
 USE_TZ = True
+
+LANGUAGE_CODE = 'en-us'
 
 
 # Static files (CSS, JavaScript, Images)
